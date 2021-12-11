8c6ea14c3f04e06da058298a23ba7d36c0940308
juancitoCH <cjuan.chona@hotmail.com>
2021-12-11 18:45:11 -0300

    Mi Primer sanp

---
+++ app.js
@@ -0,0 +1,10 @@
+import ordenarPrueba from "./vectores.js"
+import {LoadLista,crearSeccion, Enviar} from "./articulos.js"
+window.onload = main
+
+function main(){
+    document.getElementById("btnAdmin").addEventListener("click",crearSeccion)
+    ordenarPrueba()//funcion importada desde vectores.js
+    LoadLista()
+}
+
---
+++ articulos.js
@@ -0,0 +1,47 @@
+export function LoadLista(){
+   
+    let p =""
+    axios.get("http://192.168.18.28:3000/lista")
+    .then(lista=>{
+        for(let juego of lista.data)
+        {
+            p+=`<article>
+            <h2>${juego.nombre}</h2>
+            <p>${juego.descripcion}</p>
+            <div>
+            <img src="/1-Juegos/resources/images/${juego.nombreFoto}" alt="${juego.nombreFoto}">            
+            </div>
+            <ul>
+                <li><a href="https://www.playstation.com" target="blank">Ps4</a></li>
+                <li><a href="">Pc</a></li>
+                <li><a href="https://www.xbox.com" target="blank">Xbox</a></li>
+            </ul>
+        </article>`
+        }
+        document.getElementById("containerPrincipal").innerHTML+= p
+    })
+    .catch(error=>{
+        //alert(error)
+        console.log(error)
+    })
+}
+export function crearSeccion(){
+    document.getElementById("containerPrincipal").innerHTML=`
+    <div id="inputDinamico">
+        <label for="">Nombre</label>
+        <input type="text" id="nombreArticulo">
+        <label for="">Descripcion</label>
+        <input type="text"  id="descripcionArticulo">
+        <label for="">Link de la Imagen</label>
+        <input type="text" id="LinkImagen"></input>
+        <button id="envioDatos"onclick="Enviar()" >Enviar</button>
+        `
+    LoadLista()
+    
+}
+export function Enviar(){
+    let nombre = document.getElementById("nombreArticulo").value
+    let descripcion =document.getElementById("descripcionArticulo").value
+    let imagen=document.getElementById("LinkImagen").value
+    // axios.put()
+}
