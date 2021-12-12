f65416d2e6252839d869e38293ff2c4986e9c02c
juancitoCH <cjuan.chona@hotmail.com>
2021-12-11 23:09:50 -0300

    incrustacion de elementos HTML de forma correcta ''

--- articulos.js
+++ articulos.js
@@ -48,29 +48,12 @@ export function Enviar(){
 
 
 export async function loadListaFetch(){
-    let p=""
     try{
         const list = await fetch("http://192.168.18.28:3000/lista")
         if(list.ok){
             console.log("Succes")
             const lista = await list.json()
-            console.log(lista)
-            lista.forEach(juego => {
-                console.log (juego)
-                p+=`<article>
-                <h2>${juego.nombre}</h2>
-                <p>${juego.descripcion}</p>
-                <div>
-                <img src="./resources/images/${juego.nombreFoto}" alt="${juego.nombreFoto}">            
-                </div>
-                <ul>
-                    <li><a href="https://www.playstation.com" target="blank">Ps4</a></li>
-                    <li><a href="">Pc</a></li>
-                    <li><a href="https://www.xbox.com" target="blank">Xbox</a></li>
-                </ul>
-                </article>`
-            });
-            document.getElementById("containerPrincipal").innerHTML+= p
+            crearArticulo(lista)            
         }
         if(!list.ok) return console.log("Error fatal xD No se encontro la direcion que se deseaba")
        
@@ -79,5 +62,52 @@ export async function loadListaFetch(){
         console.log(error)
     }
    
+}
+
+export function crearArticulo(lista){
+    
+    lista.forEach(juego => {
+        let{nombre : njuego , descripcion:desc , nombreFoto:nfoto} =juego
+        
+        const contenedor = document.querySelector('#containerPrincipal')
+        const article = document.createElement('article')
+        const h2=document.createElement('h2')
+        const p = document.createElement('p')
+        const div = document.createElement('div')
+        const img = document.createElement('img')
+        const ul= document.createElement('ul')
+        const li = document.createElement('li')
+        const aps4 = document.createElement('a')
+        const axbox = document.createElement('a')
+        const apc = document.createElement('a')
+
+        h2.textContent = njuego
+        p.textContent = desc
+        img.src=`./resources/images/${nfoto}`
+
+        aps4.href = "https://www.playstation.com"
+        aps4.target ="blank"
+        aps4.textContent="Ps4"
+
+        axbox.href = "https://www.xbox.com"
+        axbox.target ="blank"
+        axbox.textContent="Xbox"
+
+        apc.href = "https://www.playstation.com"
+        apc.target ="blank"
+        apc.textContent="Pc"
+
+        contenedor.appendChild(article)
+        article.appendChild(h2)
+        article.appendChild(p)
+        article.appendChild(div)
+        div.appendChild(img)
+        article.appendChild(ul)
+        ul.appendChild(li)
+        li.appendChild(aps4)
+        li.appendChild(axbox)
+        li.appendChild(apc)
+    });
+    
 }
 
